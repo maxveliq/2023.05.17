6f36a7fc186454d90ce3297eaefafaab1b5c3eff
maxveliq <maxveliq@gmail.com>
2023-08-24 15:06:03 +0200

    2023.05.17
    2023.05.17

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById("addButton").addEventListener("click", addSquare);
+const wrapper = document.getElementById("wrapper");
+let counter = 1007;
+
+function addSquare() {
+    counter = counter -7;
+    let square = document.createElement("div");
+
+    if (counter % 5 === 0) {
+        square.className = "circle";
+    } else {
+        square.className = "square";
+    }
+
+    square.textContent = counter;
+    wrapper.appendChild(square);
+}
